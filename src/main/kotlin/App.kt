1e2992c95ce970f9477abdd529a70e5f0c0bfac2
Mihail Rain Heart <mihail.rain.heart@gmail.com>
2023-09-18 22:58:58 +0300

    add test purchase

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,13 @@
 import Config.BOT_TOKEN
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.dispatcher.*
+import com.github.kotlintelegrambot.dispatcher.message
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.payments.LabeledPrice
+import com.github.kotlintelegrambot.entities.payments.PaymentInvoiceInfo
+import com.github.kotlintelegrambot.logging.LogLevel
+import java.math.BigInteger
 
 class App {
 }
@@ -10,11 +15,41 @@ class App {
 fun main() {
     println("hello")
     val bot = bot {
+        timeout = 60
         token = BOT_TOKEN
+        logLevel = LogLevel.All(
+            networkLogLevel = LogLevel.Network.Body
+        )
 
         dispatch {
             text {
                 bot.sendMessage(ChatId.fromId(message.chat.id), text = "from bot: $text")
+            }
+            message {
+                bot.sendInvoice(
+                    chatId = ChatId.fromId(message.chat.id),
+                    paymentInvoiceInfo = PaymentInvoiceInfo(
+                        title = "Какой-то товар",
+                        description = "Какое-то описание",
+                        payload = "payload",
+                        providerToken = "381764678:TEST:66840",
+                        startParameter = "start",
+                        currency = "RUB",
+                        prices = listOf(LabeledPrice(label = "\u20BD", amount = BigInteger.valueOf(10158L)))
+                    ),
+                    disableNotification = true,
+                    replyToMessageId = message.messageId,
+                    allowSendingWithoutReply = false,
+                    replyMarkup = null
+                )
+            }
+
+            preCheckoutQuery {
+                println("callback: ${update.message} ${preCheckoutQuery}")
+                bot.answerPreCheckoutQuery(
+                    preCheckoutQueryId = preCheckoutQuery.id,
+                    true
+                )
             }
         }
     }
